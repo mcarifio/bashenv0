683af721eca8f6686c336c450f8e598e31bd45da
Mike Carifio <forward+git@carif.io>
2020-12-14 12:50:39 -0500

    moved install lib into rest/fn and bootstrap the installer

--- bash_login.d/rest/fn/go.fn.sh
+++ bash_login.d/rest/fn/go.fn.sh
@@ -2,9 +2,40 @@ function go.__template__ {
     local _self=${FUNCNAME[0]}
     echo ${_self} tbs
 }
-export -f go.__template__
+export -f $_
+
+function go.bootstrap {
+    sudo.snap.install --classic go && go.mkgopath    
+}
+export -f $_
+
+function go.mkgopath {
+    local _gopath=$(go env GOPATH)
+    [[ -z "${_gopath}" ]] && f.err '$(go env GOPATH) failed'
+    file.mkdir ${_gopath}/{bin,pkg,src} && go.check
+}
+export -f $_
+
+function go.i1 {
+    go install -i $*
+}
+export -f $_
+
+function go.install {
+    f.apply go.i1 $*
+}
+export -f $_
+
+
+function go.install.all {
+    go.bootstrap
+    go.install go-t
+}
+export -f $_
+
 
 function go.check {
     f.apply file.is.dir $(go env GOPATH)/{src,pkg,bin} || f.err "missing directories in $(go env GOPATH)"
 }
-export -f go.check
+export -f $_
+
--- bash_login.d/rest/fn/py.fn.sh
+++ bash_login.d/rest/fn/py.fn.sh
@@ -1,28 +1,55 @@
-# `python -m pip install --user ${package}` can sometimes install command line binaries 
-# TODO mike@carif.io: do several python installs share the same site user-base?
-# https://packaging.python.org/tutorials/installing-packages/#id21
-# path_if_exists $(python -m site --user-base)/bin
-
 function py.__template__ {
     local _self=${FUNCNAME[0]}
     echo ${_self} tbs
 }
-export -f py.__template__
+export -f $_
+
+
+function py.bootstrap {
+    local _self=${FUNCNAME[0]}
+    apt.install python3
+}
+export -f $_
+
+
+# `python -m pip install --user ${package}` can sometimes install command line binaries 
+# TODO mike@carif.io: do several python installs share the same site user-base?
+# https://packaging.python.org/tutorials/installing-packages/#id21
+# path_if_exists $(python -m site --user-base)/bin
+function py.site.bin {
+    local _self=${FUNCNAME[0]}
+    local _user_base=$(python3 -m site --user-base) # always returns a directory?
+    [[ -n "${_user_base}" ]] && file.is.dir "${_user_base}" && echo ${_user_base}/bin
+}
+export -f $_
+
+
+
+function py.pip+install {
+    local _self=${FUNCNAME[0]}
+    if [[ -z "${update_pip}" ]] ; then
+        python -m pip install --upgrade pip
+        update_pip=$(date -Iseconds)
+    fi
+    python3 -m pip install --upgrade "$*"
+}
+export -f $_
+
+function py.pip+install.start {
+    local _self=${FUNCNAME[0]}
+    pip.install wheel git git-url-parse gitpython dotmap fire rich dotmap maya fabric patchwork invocations poetry 'xonsh[full]' prompt_toolkit xfr
+}
+export -f $_
 
 
 function py.env.PYTHONSTARTUP {
     local _self=${FUNCNAME[0]}
-    u.env -r ${1:-${BASHENV}/rc/python3rc.py}
+    u.env -r $(py.env.rc)
 }
-export -f py.env.PYTHONSTARTUP
+export -f $_
 
-# less typing
-function py.pip+install {
-    local _self=${FUNCNAME[0]}    
-    if [[ -z "${update_pip}" ]] ; then
-        python -m pip install --upgrade pip
-        export update_pip=$(date -Iseconds)
-    fi
-    python -m pip install --upgrade "$*"
-}
-export -f py.pip+install 
+function py.env.rc {
+    local _py_env_rc=$(f.first (f.filter file.is.read $1 ${BASHENV:-${XDG_DATA_DIR:-~/.local/share}/bashenv}/rc/python/python3rc.py))
+    [[ -n "${_py_env_rc}" ]] && echo "${_py_env_rc}"
+} 
+export -f $_
