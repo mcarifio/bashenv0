720aa177e96ccb39d7ea83d89969eed692f5a6f2
Mike Carifio <forward+git@carif.io>
2020-12-27 13:21:37 -0500

    add directions at end of USER.sh

--- fs/user/home/USER/USER.sh
+++ fs/user/home/USER/USER.sh
@@ -5,9 +5,24 @@ _here=$(dirname ${_me})
 _basename=$(basename ${_me})
 _name=$(basename ${_me} .sh)
 
+
+# usage: [BASHENV=''] source USER.sh --echo [--force] [--systemd]
+
 # --force --echo --systemd
 if [[ -z "${BASHENV}" ]] ; then
+    # load all the bash "modules"
     source ${_here}/../../../../load.mod.sh
+
+    # using the path module, bootstrap PATH
     path.add $(path.bins)
 fi
+
+# Graft USER/{.config,.local/share} to ${HOME} via symbolic links.
+# There should be a more elegant way, perhaps a union fs?
+
 ${_here}/ln.sh $@
+
+# .local/share/*/bin may have been grafted. Add those paths as well.
+path.add $(path.bins)
+
+>&2 echo "Try logging in (--login) via ssh or a virtual terminal"
