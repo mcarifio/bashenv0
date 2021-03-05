e169ad60787133a9818e8030c91fdb0ceddeb443
Mike Carifio <forward+git@carif.io>
2021-03-04 20:36:01 -0500

    Add deno usage to bashenv

---
+++ bin.d/deno/bin/deno_install_root.ts
@@ -0,0 +1,14 @@
+#!/usr/bin/env -S deno --quiet --unstable run 
+// Mike Carifio <mike@carif.io>
+// Place this script in the deno bin directory, add that directory to path and then use it to assign DENO_INSTALL_ROOT,
+//   which will be ${DENO_DIR}/.. (depending on the shell):
+// bash:
+// export DENO_INSTALL_ROOT=$(deno_install_root.ts)
+// export DENO_DIR=$(deno_dir.ts)
+
+// https://stackoverflow.com/questions/61829367/node-js-dirname-filename-equivalent-in-deno
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/slice
+
+// Remove trailing slash.
+let here = new URL('.', import.meta.url).pathname.slice(0,-1);
+console.log(here);
